b675d4ae91c6cba577559fdba38f5b58b4f92ae3
Kirill-Kor3 <kzwork3@gmail.com>
2026-06-18 10:15:20 +0500

    fix: express wildcard route

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "*"
+  origin: "/*"
 }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
